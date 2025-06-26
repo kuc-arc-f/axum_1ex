addf44cbeae81add7ed5b2486519751b0ccda7b0
中島浩二 <knaka0209@gmail.com>
2025-06-26 15:56:55 +0900

    up, 0626a9

--- react1/src/client.tsx
+++ react1/src/client.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function App() {
   return (
   <div>
-    <h1>Hello</h1>
+    <h1 className="text-3xl font-bold">Hello</h1>
     <hr />
     <span>welcome , Rust axum +  React</span>
   </div>
--- react1/src/main.rs
+++ react1/src/main.rs
@@ -23,9 +23,13 @@ async fn main() {
 }
 
 async fn root() -> Html<&'static str> {
-    let s1 = "<html>
+    let s1 = "<!doctype html>
+<html>
   <head>
+    <meta charset='UTF-8' />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
     <title>welcome</title>
+    <script src='https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4'></script>
   </head>
   <body>
     <div id='app'></div>
